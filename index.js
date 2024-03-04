2fbaf8da81be97a0241754083c7ddb6c97d49002
Rakesh Biswas <rbiswas01999@gmail.com>
2024-03-04 14:53:14 +0600

    new file added

--- index.js
+++ index.js
@@ -99,14 +99,6 @@ const displayPosts = posts => {
 
 // latest posts
 
-// function handlerCard(){
-    
-//     fetch('https://openapi.programming-hero.com/api/retro-forum/latest-posts')
-//     .then(response => response.json())
-//     .then(data=>Showdetails(data))
-
-// }
-
 
 
 
