02acf0205e5bc5b7099e0a66529d300c3616a9e3
Rakesh Biswas <rbiswas01999@gmail.com>
2024-03-04 14:52:24 +0600

    new js file created

--- index.js
+++ index.js
@@ -107,30 +107,6 @@ const displayPosts = posts => {
 
 // }
 
-// function Showdetails(CardInfo){
-//    const CardDetailsShow = document.getElementById('CardDetailsShow')
-//    CardInfo.forEach(result=>{
-//        const div = document.createElement('section')
-//        div innerHTML = `
-//        <div class="card w-96 shadow-xl">
-//        <figure><img src="${result.cover_image}" alt="Shoes" /></figure>
-//        <div class="card-body">
-//          <h2 class="card-title">${result.author.posted}</h2>
-//          <p class="font-bold">${result.title}</p>
-//          <p class="">${result.description}</p>
-//          <div class="flex space-x-8">
-//          <img src="${result.profile_image}" class="w-24 h-24 rounded-4xl " alt="">
-//          <div>
-//          <p class="font-bold">${result.author.name}</p>  
-//          <p>${result.author.designation}</p>
-//          </div>
-//          </div>
-//        </div>
-//      </div>
-//       `
-//       CardDetailsShow.appendChild(div)
-//    })
-// }
 
 
 
